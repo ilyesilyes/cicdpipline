87d6f0c08320f29a5593a1cdbc172f4c528725fa
ilyesilyes <layes648@gmail.com>
2024-02-05 17:20:00 +0100

    initialise spring projet

---
+++ src/main/java/fr/layes/group/cicdpipeline/web/TestController.java
@@ -0,0 +1,20 @@
+package fr.layes.group.cicdpipeline.web;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+import static java.util.Map.of;
+
+@RestController
+@RequestMapping(path = "/test")
+public class TestController {
+
+    @GetMapping
+    public ResponseEntity<Map<String, String>> test() {
+        return ResponseEntity.ok().body(of("Testing", "up and running"));
+    }
+}
